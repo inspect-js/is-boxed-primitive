f0c35be63919ad6846fc9910377d6c1abda8e9b2
Jordan Harband <ljharb@gmail.com>
2019-08-10 15:31:52 -0700

    [Refactor] use `which-boxed-primitive` instead of individual predicates

--- implementation.js
+++ implementation.js
@@ -1,15 +1,7 @@
 'use strict';
 
-var isString = require('is-string');
-var isNumber = require('is-number-object');
-var isBoolean = require('is-boolean-object');
-var isSymbol = require('is-symbol');
-var isBigInt = require('is-bigint');
+var whichBoxedPrimitive = require('which-boxed-primitive');
 
 module.exports = function isBoxedPrimitive(value) {
-	if (!value || typeof value !== 'object') {
-		return false;
-	}
-
-	return isString(value) || isNumber(value) || isBoolean(value) || isSymbol(value) || isBigInt(value);
+	return typeof whichBoxedPrimitive(value) === 'string';
 };
